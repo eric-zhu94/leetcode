51af82596344ddba47cbd605f2719150d3c47629
Eric Zhu <eric.zhu94@gmail.com>
2020-04-01 16:35:32 +1100

    Create 1342-Number-of-Steps-to-Reduce-a-Number-to-Zero.py

---
+++ 1342-Number-of-Steps-to-Reduce-a-Number-to-Zero.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numberOfSteps (self, num):
+        count = 0
+        while num > 0:
+            if num % 2 == 0:
+                count +=1
+                num = num // 2
+            else:
+                num -= 1
+                count += 1
+        return count
+        """
+        :type num: int
+        :rtype: int
+        """
+        
