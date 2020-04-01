cc3ec34dbfbaf5f329f70285fa04702a4cbb4798
Eric Zhu <eric.zhu94@gmail.com>
2020-04-01 16:33:04 +1100

    Create 1108-Defanging-an-IP-Address.py

---
+++ 1108-Defanging-an-IP-Address.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def defangIPaddr(self, address):
+        return address.replace(".","[.]")
+        
+        """
+        :type address: str
+        :rtype: str
+        """
+        
