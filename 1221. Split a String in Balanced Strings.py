962af701bf114e59d764ae05cf7ba0e71b657102
Eric Zhu <eric.zhu94@gmail.com>
2020-04-01 16:50:10 +1100

    Create 1221. Split a String in Balanced Strings.py

---
+++ 1221. Split a String in Balanced Strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        stack = []
+        cnt = 0
+        for c in s:
+            if c == 'R':
+                stack.append()
+
+        
