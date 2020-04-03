ba693ecfd68f84a158951133ed774c8ae827f0d9
Eric Zhu <eric.zhu94@gmail.com>
2020-04-04 09:52:44 +1100

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         hi = float('-inf')
-        ptr = 0
         local_max = 0
         for i in range(len(nums)):
             local_max = max(nums[i], nums[i] + local_max)
