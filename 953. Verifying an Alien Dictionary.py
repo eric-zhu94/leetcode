8d22652ce8a38157d62df019babadcf5f50bec47
Eric Zhu <eric.zhu94@gmail.com>
2021-03-23 21:40:05 +1100

    Update 953. Verifying an Alien Dictionary.py

--- 953. Verifying an Alien Dictionary.py
+++ 953. Verifying an Alien Dictionary.py
@@ -1,28 +1,40 @@
 #https://leetcode.com/problems/verifying-an-alien-dictionary/
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        hashmap = {k:v for v,k in enumerate(order)}
-        def compare(first, second, order):
-            ptr = 0
-            shorter = first if len(first) < len(second) else second
-            while ptr < len(shorter):
-                if order[second[ptr]] == order[first[ptr]]:
-                    ptr += 1
-                elif order[second[ptr]] < order[first[ptr]]:
+        ordering = {}
+        tmp = list(order)
+        for i in range(len(tmp)):
+            ordering[tmp[i]] = i
+        for j in range(1, len(words)):
+            first = words[j - 1]
+            second = words[j]
+            length = min(len(first), len(second))
+            first_ptr = 0
+            second_ptr = 0
+            #Scenario 1: characters are same, increment and compare
+            #Scenario 2: characters are different, w1 < w2, continue
+            #Scenario 3: characters are different, w1 > w2, return false
+            #Scenario 4: reached end of a word, compare length
+            while first[first_ptr] == second[second_ptr]:
+                #move to first different character
+                first_ptr +=1
+                second_ptr +=1
+                if first_ptr >= length:
+                    if len(first) > len(second):
+                        return False
+                    else:
+                        break
+            while first_ptr < length:
+                if ordering[first[first_ptr]] > ordering[second[second_ptr]]:
                     return False
                 else:
-                    return True
-            if shorter == first:
-                return True
-            return False
-            
-        start = 0
-        end = 1
-        while end < len(words):
-            if not compare(words[start],words[end],hashmap):
-                return False
-            start +=1
-            end +=1
+                    break
+            if first_ptr >= length:
+                if len(first) > len(second):
+                    return False
+                else:
+                    break
+
         return True
             
             
